8347218d29118f06398fe8d6c97aa84ece0d604e
beefjerks <kari051s@easv365.dk>
2021-05-06 18:25:32 +0200

    got rid of stupid THING

--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -56,14 +56,12 @@
       </v-col>
        
 
-
-
-      <v-col  cols="12" md="4" class="product">
-        <v-card class="pa-4" >
-                <v-img src="@/assets/Slideshow_2.png" height="auto"></v-img>
-                  <v-card-title class="justify-center">
-                    OceanEye Website 
-                  </v-card-title>
+      <!-- <v-col  cols="12" md="4" class="product">
+          <v-card class="pa-4" max-height="375px">
+            <v-img class="onlyLogo" src="@/assets/onlyLogo.png"></v-img>
+              <v-card-title class="justify-center">
+                OceanEye project 
+              </v-card-title>
           <v-row justify="space-around">
             <v-col cols="auto">
             <v-dialog
@@ -82,7 +80,64 @@
                   </v-toolbar>
                  <v-card-text>
                       <v-row class="pa-4">
-                        <v-col >
+                        <v-col>
+                          <img src="@/assets/Slideshow_1.png">
+                        </v-col>
+                      
+                      <v-col>
+                          <img src="@/assets/Slideshow_2.png">
+                          </v-col>
+
+                          <v-col>
+                          <img src="@/assets/Slideshow_3.png">
+                          </v-col>
+
+                          <v-col>
+                          <img src="@/assets/Slideshow_4.png">
+                          </v-col>
+
+                          <v-col>
+                          <img src="@/assets/Slideshow_5.png">
+                          </v-col>
+                      </v-row>
+                  </v-card-text>
+                  <v-card-actions class="justify-end">
+                    <v-btn text @click="dialog.value = false">
+                      Close
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </template>
+            </v-dialog>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col> -->
+      <v-col  cols="12" md="4" class="product">
+            <v-card class="pa-4">
+            <v-img src="@/assets/onlyLogo.png" height="auto"></v-img>
+              <v-card-title class="justify-center">
+                Digital art
+              </v-card-title>
+          <v-row justify="space-around">
+            <v-col cols="auto">
+            <v-dialog
+            transition="dialog-bottom-transition"
+            max-width="900"
+            >
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn color="thirdiary" v-bind="attrs" v-on="on"> 
+                View Project
+                </v-btn>
+              </template>
+              <template v-slot:default="dialog">
+                <v-card>
+                  <v-toolbar color="thirdiary" dark>
+                    7-Kanten Project
+                  </v-toolbar>
+                  <v-card-text>
+                      <v-row class="pa-4">
+                        <v-col>
                           <img src="@/assets/Slideshow_1.png">
                         </v-col>
                       
@@ -140,7 +195,7 @@
                   </v-toolbar>
                   <v-card-text class="artN">
                       <v-row class="pa-4">
-                        <v-col>
+                        <v-col >
                           <img src="@/assets/ArtN1.png" width="150%">
                         </v-col>
                       
@@ -168,7 +223,7 @@
 
       <v-col  cols="12" md="4" class="product">
             <v-card class="pa-4">
-            <v-img src="@/assets/greenrabbit.png" height="auto"></v-img>
+            <v-img src="@/assets/greenrabbit.png" height="auto"></v-img><!-- hej -->
               <v-card-title class="justify-center">
                 Green Rabbits 
               </v-card-title>
@@ -326,6 +381,9 @@ font-family: "filson-pro";
    
 }
 
+.v-responsive__sizer{
+  padding-bottom: 100% !important /* <- very */;
+}
 
 
 
@@ -333,8 +391,22 @@ font-family: "filson-pro";
 </style>
 
 <script>
-    export default {
-        
+  export default {
+    data: () => ({
+      
+    }),
+    methods: {
+
+    },
+    mounted() {
+      function thisNeedsToStop ()  {
+        const shitFuck = document.querySelectorAll('.pa-4 > div > div')
+        console.log(shitFuck)
+      }
+
+      thisNeedsToStop()
     }
+      
+  }
 </script>
 
